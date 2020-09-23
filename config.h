8417c4c30b36bbf71021ad4759d6420ce62d11ad
Francisco Veiga <francisco2.veiga@gmail.com>
2020-09-23 12:50:29 -0300

    commit before heavy customization

--- config.h
+++ config.h
@@ -94,7 +94,9 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
+static const char *lowbrowsercmd[]  = { "qutebrowser", NULL };
 static const char *alsamixercmd[] = {"st", "-e", "alsamixer" };
+static const char *vifmcmd[] = {"st", "-e", "vifm" };
 static const char *layoutmenu_cmd = "layoutmenu.sh";
 
 /* commands spawned when clicking statusbar, the mouse button pressed is exported as BUTTON */
@@ -105,7 +107,9 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_u,      spawn,          {.v = browsercmd } },
+	{ MODKEY|ShiftMask,             XK_u,      spawn,          {.v = lowbrowsercmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = vifmcmd } },
 	{ MODKEY,                       XK_a,      spawn,          {.v = alsamixercmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
